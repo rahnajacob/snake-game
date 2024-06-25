7be10437dcd3cb7255ca668471a3354334d0b741
rjacob <rahnarjacob@gmail.com>
2024-06-26 01:29:17 +0530

    Added createGrid fn, minor CSS, minor html changes

--- code/app.js
+++ code/app.js
@@ -24,129 +24,143 @@
 /*-------------- Constants -------------*/
  const gridWidth = 20;
  const gridHeight = 20;
- const totalGridCount = gridWidth * gridHeight;
- const snakeStartingPosition = //board index of 2, length of three
- const snakeStartingDirection; //board[i + 1]
+ const totalCellCount = gridWidth * gridHeight;
+ //const snakeStartingPosition = grid[2] //grid index of 2, length of three
+ //console.log(snakeStartingPosition)
+//  const snakeStartingDirection = grid[3]; //grid[i + 1]
 
 /*---------- Variables (state) ---------*/
 
-let currentScoreValue;
-//starts as zero, +10 for every fruit consumed
+// let currentScoreValue;
+// //starts as zero, +10 for every fruit consumed
 
-//
+// let gridDisplay;
+// // starts out empty, then visual of snake movement + length updating plus randomised fruit drops 
 
-let gridDisplay;
-// starts out empty, then visual of snake movement + length updating plus randomised fruit drops 
+// let snakeArray = [];
+// // length = 3 at the start of the game, css styling of bg-color: "colour"
 
-let snakeArray = [];
-// length = 3 at the start of the game, css styling of bg-color: "colour"
+// let fruit;
+// //css styling: 1 cell of colouring, class of fruit
 
-let fruit;
-//css styling: 1 cell of colouring, class of fruit
+// let winState;
+// //set to false
 
-let winState;
-//set to false
+// let loseState;
+// //set to false
 
-let loseState;
-//set to false
+// let intervalTime;
+// //1000 or 1500ms at the beginning?
 
-let intervalTime;
-//1000 or 1500ms at the beginning?
+// let snakeColour; //OPT
 
-let snakeColour; //OPT
+// let fruitColour; //OPT
 
-let fruitColour; //OPT
-
-let highScoreValue = []; //OPT
-//starts as zero, PUSH currentScore once game has reached a win or fail state
-//look up local storage- JS, use to store the values instead
+// let highScoreValue = []; //OPT
+// //starts as zero, PUSH currentScore once game has reached a win or fail state
+// //look up local storage- JS, use to store the values instead
 
 /*----- Cached Element References  -----*/
+const grid = document.querySelector(".grid")
+console.log(grid)
+const cellEls = []
  // newGameButton (aka play/reset button)
  // resetHighScoreButton
  // OPT: toggleMusicButton
  // messageDisplay
- // gridWrapper from HTML, needed to append grid created using JS
- // individual gridDivs created using JS
+ // gridWrapper from HTML, needed to append grid created using JS (DONE)
+ // individual gridDivs created using JS (DONE)
 
- init()
+//  init()
 /*-------------- Functions -------------*/
 
 function createGrid(){
-    // for(let i = 0; i < totalGridCount; i++) {
-    //     let div = document.createElement("div")
+    for (let i = 0; i < totalCellCount; i++) {
+        let cell = document.createElement("div")
+        cell.classList.add("cell")
+        cell.setAttribute("id", i)
+        cell.innerText = i
+        cell.style.width = `${100/gridWidth}%`
+        cell.style.height = `${100/gridHeight}%`
+        cellEls.push(cell)
+        grid.appendChild(cell)
+        // let cell = document.createElement("div")
+        // cell.setAttribute("id", i)
+
     //     set individual grid square id = [i]
     //     appendChild onto grid wrapper in HTML
     // }
 }
-
-function init () {
-    // winState = false
-    // currentScore = 0
-    // toggleMusicButton.audio.mute = true
-    // render()
 }
+createGrid()
 
-function render() {
-    //update gridDisplay
-    //update messageDisplay
-}
+// function init () {
+//     // winState = false
+//     // currentScore = 0
+//     // toggleMusicButton.audio.mute = true //OPT
+//     // render()
+// }
 
-function placeFruit() {
-    //IF board has empty positions in array
-        //THEN Math.random, Math.floor(place one fruit in an empty array position)
-    //ELSE return false
-}
+// function render() {
+//     //update gridDisplay
+//     //update messageDisplay
+// }
 
-function snakeEating() {
-    //IF current snake array contains class of fruit, remove fruit and +1 to snake array?
-    //IF position of snake[0] on the board = position of fruit on the board, add points?
-    //snakeArray.push(one more unit)
-    //delete fruit from the board index
-    //currentScore()
-}
+// function placeFruit() {
+//     //IF grid has empty positions in array
+//         //THEN Math.random, Math.floor(place one fruit in an empty array position)
+//     //ELSE return false
+// }
 
-function snakeMovement(e) {
-    //arrowButton = e.target.key (confirm whether key or keyCode)
-    //IF left: snake[0]at grid position moves to board[i - 1]
-        //AND board[i] % boardWidth != 0
-    //IF right: moves to board[i] +1
-        //AND board[i] % boardWidth != boardWidth - 1
-    //IF up: moves to board[i] - gridWidth
-        //AND board[i] >= 0
-    //IF down: moves by board[i] + gridWidth
-        //AND board[i] <= totalGridCount - 1
-    //ELSE: snake has hit border wall, lose state = true
-    //UNSHIFT at head + POP at tail
-}
+// function snakeEating() {
+//     //IF current snake array contains class of fruit, remove fruit and +1 to snake array?
+//     //IF position of snake[0] on the grid = position of fruit on the grid, add points?
+//     //snakeArray.push(one more unit)
+//     //delete fruit from the board index
+//     //currentScore()
+// }
 
-function currentScore() { 
-    //adds 10 pts for every fruit
-    //placeFruit()
-}
+// function snakeMovement(e) {
+//     //arrowButton = e.target.key (confirm whether key or keyCode)
+//     //IF left: snake[0]at grid position moves to grid[i - 1]
+//         //AND grid[i] % gridWidth != 0
+//     //IF right: moves to board[i] +1
+//         //AND grid[i] % gridWidth != gridWidth - 1
+//     //IF up: moves to grid[i] - gridWidth
+//         //AND grid[i] >= 0
+//     //IF down: moves by grid[i] + gridWidth
+//         //AND grid[i] <= totalGridCount - 1
+//     //ELSE: snake has hit border wall, lose state = true
+//     //UNSHIFT at head + POP at tail
+// }
 
-function checkForWinning() {
-    //IF !empty grid AND placeFruit = false, set winState = true
-    //set displayMessage = "Congratulations, you have won"
-    //PUSH currentScore.innerText to highScore
-}
+// function currentScore() { 
+//     //adds 10 pts for every fruit
+//     //placeFruit()
+// }
 
-function checkForLosing() {
-    //IF grid movement tries to go out of bounds OR if snake[0] on board array = snake[i] on board array //classList.contains - use to check if grid being moved to contains snake class (use in checkforWalls and checkforfruit?)
-    //set displayMessage = "You have lost! Would you like to try again?"
-    //PUSH currentScore.innerText to highScore
-}
+// function checkForWinning() {
+//     //IF !empty grid AND placeFruit = false, set winState = true
+//     //set displayMessage = "Congratulations, you have won"
+//     //PUSH currentScore.innerText to highScore
+// }
 
-function highScore() {
-    //loop through array values to find highest score
-    //let highScoreValue = highest score
-}
+// function checkForLosing() {
+//     //IF grid movement tries to go out of bounds OR if snake[0] on grid array = snake[i] on grid array //classList.contains - use to check if grid being moved to contains snake class (use in checkforWalls and checkforfruit?)
+//     //set displayMessage = "You have lost! Would you like to try again?"
+//     //PUSH currentScore.innerText to highScore
+// }
 
-function resetHighScore() {
-    //set highScore value = 0
-}
+// function highScore() {
+//     //loop through array values to find highest score
+//     //let highScoreValue = highest score
+// }
+
+// function resetHighScore() {
+//     //set highScore value = 0
+// }
 /*----------- Event Listeners ----------*/
-newGameButton.addEventListener("click", init)
-resetHighScoreButton.addEventListener("click", resetHighScore)
-document.addEventListener("keyup", snakeMovement)
+// newGameButton.addEventListener("click", init)
+// resetHighScoreButton.addEventListener("click", resetHighScore)
+// document.addEventListener("keyup", snakeMovement)
 
